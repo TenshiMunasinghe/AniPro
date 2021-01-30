6b89612e865355d165affa2ff4a3a2955b80dcc1
shadow-chadow <56468853+shadow-chadow@users.noreply.github.com>
2021-01-30 15:19:22 +0530

    remove file

--- src/hooks/usePrev.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react'
-
-export const usePrev = (value: any) => {
-  const ref = useRef()
-  useEffect(() => {
-    ref.current = value
-  })
-  return ref.current
-}
