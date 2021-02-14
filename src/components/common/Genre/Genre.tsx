9c43dfb2a2cf2e07b42d087037f3ac2529cd61d2
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-02-15 00:29:08 +0530

    dynamic placement of genre buttons

--- src/components/common/Genre/Genre.tsx
+++ src/components/common/Genre/Genre.tsx
@@ -1,5 +1,7 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useLayoutEffect, useRef, useState } from 'react'
+
 import { adjustColor } from '../../../utils/adjustColor'
+import { checkElementOverflow } from '../../../utils/checkElementOverflow'
 
 import styles from './Genre.module.scss'
 
@@ -11,6 +13,18 @@ interface Props {
 }
 
 export const Genre = ({ genre, onClick, color, canInteract = true }: Props) => {
+  const [isHidden, setIsHidden] = useState(false)
+  const ref = useRef<HTMLButtonElement>(null)
+
+  useLayoutEffect(() => {
+    if (isHidden) return
+    const isOverflow = checkElementOverflow(ref.current as HTMLElement)?.any
+
+    if (isOverflow) {
+      setIsHidden(true)
+    }
+  }, [isHidden])
+
   const _color = adjustColor(color, 'var(--lightness)')
   const style = {
     '--color-text': _color ? _color : 'var(--color-text-200)',
@@ -20,7 +34,8 @@ export const Genre = ({ genre, onClick, color, canInteract = true }: Props) => {
 
   return (
     <button
-      className={styles.genre}
+      ref={ref}
+      className={styles.genre + (isHidden ? ` ${styles.hide}` : '')}
       onClick={onClick}
       style={style}
       tabIndex={canInteract ? 0 : -1}>
