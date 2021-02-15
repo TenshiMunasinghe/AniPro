d39f91aab4562e15292d3c620b3e0ea36404fd5c
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-02-15 11:08:41 +0530

    remove

--- src/hooks/useSetGenre.ts
+++
@@ -1,6 +0,0 @@
-import { useHistory } from 'react-router-dom'
-
-export const useSetGenre = () => {
-  const history = useHistory()
-  return (genre: string) => history.push(`/search/?genres=${genre}`)
-}
