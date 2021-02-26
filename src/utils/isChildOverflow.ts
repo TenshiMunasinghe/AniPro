e11d66a0907fbbe1889e764b39f0e4f3ee3df8a9
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-02-26 10:35:44 +0530

    refine logic

--- src/utils/isChildOverflow.ts
+++ src/utils/isChildOverflow.ts
@@ -1,6 +1,15 @@
 export const isChildOverflow = (element: HTMLElement) => {
-  const x = element.scrollWidth - element.clientWidth
-  const y = element.scrollHeight - element.clientHeight
+  const rect = element.getBoundingClientRect()
+  const style = getComputedStyle(element)
+  const lastChild = element.lastElementChild?.getBoundingClientRect()
+  if (!lastChild)
+    return {
+      overflow: { x: false, y: false, either: false },
+      overflownAmount: { x: 0, y: 0 },
+    }
+
+  const x = lastChild.right - (rect.right - parseFloat(style.paddingRight))
+  const y = lastChild.bottom - (rect.bottom - parseFloat(style.paddingBottom))
 
   return {
     overflow: { x: x > 0, y: y > 0, either: x > 0 || y > 0 },
