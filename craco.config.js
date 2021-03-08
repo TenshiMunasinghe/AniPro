79f1c534ad714f4bab9132922dc5c861dbedeffd
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-03-08 22:38:42 +0530

    edit config

--- craco.config.js
+++ craco.config.js
@@ -2,11 +2,11 @@ module.exports = {
   webpack: {
     configure: webpackConfig => {
       const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
-        plugin => plugin.options && plugin.options.ignoreOrder != null
+        plugin => plugin.constructor.name !== 'MiniCssExtractPlugin'
       )
-
-      instanceOfMiniCssExtractPlugin.options.ignoreOrder = true
-
+      if (instanceOfMiniCssExtractPlugin) {
+        instanceOfMiniCssExtractPlugin.options.ignoreOrder = true
+      }
       return webpackConfig
     },
   },
