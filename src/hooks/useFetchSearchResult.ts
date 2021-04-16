859f1b7b47b44291e3cc1fe5b882b82ded059246
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-04-16 19:23:38 +0530

    refactor logic

--- src/hooks/useFetchSearchResult.ts
+++ src/hooks/useFetchSearchResult.ts
@@ -1,73 +1,71 @@
 import uniqBy from 'lodash/uniqBy'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { URLSearchParams } from 'url'
 
 import { GET_SEARCH_RESULT, ky } from '../api/queries'
-import { NextPageInfo, QueryData, QueryVar, SearchResult } from '../api/types'
+import { PageInfo, QueryData, SearchResult } from '../api/types'
+import { allowedURLParams } from '../filterOptions/filterOptions'
 
-type FetchAnimesParam = { queryVariables: Partial<QueryVar> }
-interface FetchDataParam extends FetchAnimesParam {
-  paginate: boolean
-}
-
-export const useFetchSearchResult = ({ queryVariables }: FetchAnimesParam) => {
+export const useFetchSearchResult = (params: URLSearchParams) => {
   const [medias, setMedias] = useState<SearchResult[] | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState()
 
-  const nextPageInfo = useRef<NextPageInfo>({
+  const pageInfo = useRef<PageInfo>({
     currentPage: 1,
     hasNextPage: false,
+    lastPage: 1,
   })
 
   const isMounted = useRef(false)
 
-  const fetchData = useCallback(
-    async ({ queryVariables, paginate }: FetchDataParam) => {
-      const page = paginate ? nextPageInfo.current.currentPage + 1 : 1
-      try {
-        if (!paginate) setMedias(null)
+  const fetchData = useCallback(async (params: URLSearchParams) => {
+    try {
+      const queryVariables = Object.fromEntries(
+        Array.from(params.entries()).filter(([key]) =>
+          allowedURLParams.includes(key)
+        )
+      )
+      setLoading(true)
+      setMedias(null)
 
-        setLoading(true)
-
-        const res: { data: QueryData } = await ky
-          .post('', {
-            json: {
-              query: GET_SEARCH_RESULT,
-              variables: {
-                ...queryVariables,
-                perPage: queryVariables.perPage || 20,
-                page,
-              },
+      const res: { data: QueryData } = await ky
+        .post('', {
+          json: {
+            query: GET_SEARCH_RESULT,
+            variables: {
+              ...queryVariables,
+              sortBy: queryVariables.sortBy || 'TRENDING_DESC',
+              perPage: queryVariables.perPage || 20,
             },
-          })
-          .json()
-
-        if (!res || !isMounted.current) {
-          return
-        }
-
-        const {
-          data: { Page },
-        } = res
-
-        setMedias(prev => {
-          if (paginate && prev) {
-            return uniqBy([...prev, ...Page.media], 'id')
-          } else {
-            return res.data.Page.media
-          }
+          },
         })
-        nextPageInfo.current = { ...Page.pageInfo }
-      } catch (e) {
-        if (isMounted.current) {
-          setError(e)
-        }
-        console.error(e)
+        .json()
+
+      if (!res || !isMounted.current) {
+        return
       }
-      setLoading(false)
-    },
-    []
-  )
+
+      const {
+        data: { Page },
+      } = res
+
+      setMedias(prev => {
+        if (prev) {
+          return uniqBy([...prev, ...Page.media], 'id')
+        } else {
+          return res.data.Page.media
+        }
+      })
+      pageInfo.current = { ...Page.pageInfo }
+    } catch (e) {
+      if (isMounted.current) {
+        setError(e)
+      }
+      console.error(e)
+    }
+    setLoading(false)
+  }, [])
 
   useEffect(() => {
     isMounted.current = true
@@ -77,14 +75,14 @@ export const useFetchSearchResult = ({ queryVariables }: FetchAnimesParam) => {
   }, [])
 
   useEffect(() => {
-    fetchData({ queryVariables, paginate: false })
-  }, [queryVariables, fetchData])
+    fetchData(params)
+  }, [params, fetchData])
 
   return {
     medias,
     loading,
     error,
     fetchData,
-    nextPageInfo: nextPageInfo.current,
+    pageInfo: pageInfo.current,
   }
 }
