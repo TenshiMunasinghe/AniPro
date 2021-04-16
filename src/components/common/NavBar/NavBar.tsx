21e52f99adc979e33c1732c3a8a8cff4c97524e0
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-04-16 19:27:58 +0530

    remove unused var

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,5 +1,4 @@
-import classnames from 'classnames'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback } from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
