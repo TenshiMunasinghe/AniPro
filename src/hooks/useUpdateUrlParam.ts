68ca244113726081d9407834364ac15b90632c34
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-04-30 23:04:08 +0530

    prevent updating params when loading

--- src/hooks/useUpdateUrlParam.ts
+++ src/hooks/useUpdateUrlParam.ts
@@ -2,12 +2,15 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 
 import { QueryVar } from '../api/types'
+import { LoadingStore, useLoadingStore } from '../zustand/stores'
 
 type setParamArg = {
   params: URLSearchParams
   value: string | number | string[]
   key: string
 }
+
+const loadingSelector = (state: LoadingStore) => state.loadingSearchResult
 
 const addParam = ({ params, value, key }: setParamArg) => {
   String(value).length !== 0
@@ -18,6 +21,9 @@ const addParam = ({ params, value, key }: setParamArg) => {
 export const useUpdateUrlParam = () => {
   const history = useHistory()
   const location = useLocation()
+
+  const loading = useLoadingStore(loadingSelector)
+
   const initialParams = useMemo(() => new URLSearchParams(location.search), [
     location.search,
   ])
@@ -42,6 +48,7 @@ export const useUpdateUrlParam = () => {
 
   const addFilterOptions = useCallback(
     (queryVars: Partial<QueryVar>, willApply: boolean) => {
+      if (loading) return
       setParams(prev => {
         const params = new URLSearchParams(prev.query)
 
@@ -62,16 +69,17 @@ export const useUpdateUrlParam = () => {
         return { query: params.toString(), willApply }
       })
     },
-    []
+    [loading]
   )
 
   const applyFilter = useCallback(() => {
+    if (loading) return
     setParams(prev => {
       const params = new URLSearchParams(prev.query)
       params.set('page', '1')
       return { query: params.toString(), willApply: true }
     })
-  }, [])
+  }, [loading])
 
   return {
     addFilterOptions,
