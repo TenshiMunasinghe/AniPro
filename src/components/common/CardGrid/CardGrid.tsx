a91c48c7490d8d23f3e60e0d3896d8bde2537271
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-05-22 10:26:29 +0530

    implement context for scrollPosition

--- src/components/common/CardGrid/CardGrid.tsx
+++ src/components/common/CardGrid/CardGrid.tsx
@@ -1,8 +1,9 @@
 import classnames from 'classnames'
 import range from 'lodash/range'
-import React, { useMemo } from 'react'
+import React, { createContext, useMemo } from 'react'
 import {
   LazyComponentProps,
+  ScrollPosition,
   trackWindowScroll,
 } from 'react-lazy-load-image-component'
 
@@ -35,6 +36,10 @@ interface Props extends LazyComponentProps {
 }
 
 const PAGES = [-2, -1, 0, 1, 2]
+
+export const ScrollPositionContext = createContext<ScrollPosition | undefined>(
+  undefined
+)
 
 const CardGrid = ({
   params,
@@ -74,118 +79,120 @@ const CardGrid = ({
   }
 
   return (
-    <div
-      className={classnames(styles.wrapper, {
-        [styles.sideScroll]: sideScroll,
-      })}>
-      <section className={classnames(styles.slider, styles[cardType])}>
-        {_medias &&
-          _medias.map(m => {
-            switch (cardType) {
-              case 'cover':
-                return (
-                  <CardCover
-                    key={m.id}
-                    id={m.id}
-                    image={m.coverImage[imageSize]}
-                    color={m.coverImage.color}
-                    title={m.title}
-                    genres={m.genres}
-                    status={m.status}
-                    nextAiringEpisode={m.nextAiringEpisode}
-                    format={m.format}
-                    season={m.season}
-                    seasonYear={m.seasonYear}
-                    episodes={m.episodes}
-                    duration={m.duration}
-                    meanScore={m.meanScore}
-                    studios={m.studios}
-                    rank={m.rank}
-                    scrollPosition={scrollPosition}
-                  />
-                )
+    <ScrollPositionContext.Provider value={scrollPosition}>
+      <div
+        className={classnames(styles.wrapper, {
+          [styles.sideScroll]: sideScroll,
+        })}>
+        <section className={classnames(styles.slider, styles[cardType])}>
+          {_medias &&
+            _medias.map(m => {
+              switch (cardType) {
+                case 'cover':
+                  return (
+                    <CardCover
+                      key={m.id}
+                      id={m.id}
+                      image={m.coverImage[imageSize]}
+                      color={m.coverImage.color}
+                      title={m.title}
+                      genres={m.genres}
+                      status={m.status}
+                      nextAiringEpisode={m.nextAiringEpisode}
+                      format={m.format}
+                      season={m.season}
+                      seasonYear={m.seasonYear}
+                      episodes={m.episodes}
+                      duration={m.duration}
+                      meanScore={m.meanScore}
+                      studios={m.studios}
+                      rank={m.rank}
+                      scrollPosition={scrollPosition}
+                    />
+                  )
 
-              case 'table':
-                return (
-                  <CardTable
-                    key={m.id}
-                    id={m.id}
-                    image={{
-                      cover: m.coverImage[imageSize],
-                      banner: m.bannerImage,
-                    }}
-                    color={m.coverImage.color}
-                    title={m.title}
-                    genres={m.genres}
-                    status={m.status}
-                    nextAiringEpisode={m.nextAiringEpisode}
-                    format={m.format}
-                    season={m.season}
-                    seasonYear={m.seasonYear}
-                    episodes={m.episodes}
-                    duration={m.duration}
-                    meanScore={m.meanScore}
-                    studios={m.studios}
-                    popularity={m.popularity}
-                    rank={m.rank}
-                    scrollPosition={scrollPosition}
-                  />
-                )
+                case 'table':
+                  return (
+                    <CardTable
+                      key={m.id}
+                      id={m.id}
+                      image={{
+                        cover: m.coverImage[imageSize],
+                        banner: m.bannerImage,
+                      }}
+                      color={m.coverImage.color}
+                      title={m.title}
+                      genres={m.genres}
+                      status={m.status}
+                      nextAiringEpisode={m.nextAiringEpisode}
+                      format={m.format}
+                      season={m.season}
+                      seasonYear={m.seasonYear}
+                      episodes={m.episodes}
+                      duration={m.duration}
+                      meanScore={m.meanScore}
+                      studios={m.studios}
+                      popularity={m.popularity}
+                      rank={m.rank}
+                      scrollPosition={scrollPosition}
+                    />
+                  )
 
-              case 'chart':
-                return (
-                  <CardChart
-                    key={m.id}
-                    id={m.id}
-                    image={m.coverImage[imageSize]}
-                    color={m.coverImage.color}
-                    title={m.title}
-                    genres={m.genres}
-                    description={m.description}
-                    meanScore={m.meanScore}
-                    scrollPosition={scrollPosition}
-                  />
-                )
+                case 'chart':
+                  return (
+                    <CardChart
+                      key={m.id}
+                      id={m.id}
+                      image={m.coverImage[imageSize]}
+                      color={m.coverImage.color}
+                      title={m.title}
+                      genres={m.genres}
+                      description={m.description}
+                      meanScore={m.meanScore}
+                      scrollPosition={scrollPosition}
+                    />
+                  )
 
-              default:
-                return <></>
-            }
-          })}
-        {isLoading &&
-          range(0, loadingCount).map((_, i) => (
-            <CardLoading type={cardType} key={i} />
-          ))}
-      </section>
-      {!isError && !isLoading && hasPages && (
-        <section className={styles.pages}>
-          <button className={styles.page} onClick={() => redirectToPage(1)}>
-            {'<<'}
-          </button>
-          {PAGES.map(p => {
-            const page = pageInfo.currentPage + p
-
-            if (page <= 0 || page > pageInfo.lastPage) return null
-
-            return (
-              <button
-                key={page}
-                className={classnames(
-                  { [styles.current]: pageInfo.currentPage === page },
-                  styles.page
-                )}
-                onClick={() => redirectToPage(page)}>
-                {page}
-              </button>
-            )
-          })}
-          <button
-            className={styles.page}
-            onClick={() => redirectToPage(pageInfo.lastPage)}>
-            {'>>'}
-          </button>
+                default:
+                  return <></>
+              }
+            })}
+          {isLoading &&
+            range(0, loadingCount).map((_, i) => (
+              <CardLoading type={cardType} key={i} />
+            ))}
         </section>
-      )}
-    </div>
+        {!isError && !isLoading && hasPages && (
+          <section className={styles.pages}>
+            <button className={styles.page} onClick={() => redirectToPage(1)}>
+              {'<<'}
+            </button>
+            {PAGES.map(p => {
+              const page = pageInfo.currentPage + p
+
+              if (page <= 0 || page > pageInfo.lastPage) return null
+
+              return (
+                <button
+                  key={page}
+                  className={classnames(
+                    { [styles.current]: pageInfo.currentPage === page },
+                    styles.page
+                  )}
+                  onClick={() => redirectToPage(page)}>
+                  {page}
+                </button>
+              )
+            })}
+            <button
+              className={styles.page}
+              onClick={() => redirectToPage(pageInfo.lastPage)}>
+              {'>>'}
+            </button>
+          </section>
+        )}
+      </div>
+    </ScrollPositionContext.Provider>
   )
 }
 
