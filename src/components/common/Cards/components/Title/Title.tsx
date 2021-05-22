2ae3c1f4421e2f3b053ab47e5b2b98ee1f5de779
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-05-22 10:01:54 +0530

    initial commit

---
+++ src/components/common/Cards/components/Title/Title.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Title.module.scss'
+import { linkToMediaPage } from '../../../../../App'
+import { adjustColor } from '../../../../../utils/adjustColor'
+
+interface Props {
+  id: number
+  text: string
+  color: string
+}
+
+const Title = ({ id, text, color }: Props) => {
+  const style = {
+    '--color-text': adjustColor(color, 'var(--lightness)'),
+  } as React.CSSProperties
+
+  return (
+    <Link to={linkToMediaPage(id)} className={styles.link} style={style}>
+      {text}
+    </Link>
+  )
+}
+
+export default Title
