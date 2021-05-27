00586c72fd849c4dfe004d6efdc7b7caff9545e7
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-05-27 18:10:53 +0530

    initial commit

---
+++ src/components/common/Cards/components/Score/Score.tsx
@@ -0,0 +1,17 @@
+import FaceIcon from '../../../FaceIcon/FaceIcon'
+import styles from './Score.module.scss'
+
+interface Props {
+  score: number
+}
+
+const Score = ({ score }: Props) => {
+  return (
+    <div className={styles.wrapper}>
+      <FaceIcon meanScore={score} />
+      <span className={styles.score}>{score}%</span>
+    </div>
+  )
+}
+
+export default Score
