a7d6e75637293e3607780b6069f4f5d40d20cc01
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-07-22 22:12:11 +0530

    lazy load image

--- src/components/media/Person/Person.tsx
+++ src/components/media/Person/Person.tsx
@@ -1,4 +1,7 @@
 import classnames from 'classnames'
+import { useContext } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { context } from '../../../pages/media/Media'
 import { toStartCase } from '../../../utils/toStartCase'
 import styles from './Person.module.scss'
 
@@ -10,12 +13,18 @@ interface Props {
 }
 
 const Person = ({ image, name, info, isReversed = false }: Props) => {
+  const { scrollPosition } = useContext(context)
   return (
     <div
       className={classnames(styles.container, {
         [styles.reversed]: isReversed,
       })}>
-      <img src={image} alt={name} className={styles.image} />
+      <LazyLoadImage
+        scrollPosition={scrollPosition}
+        src={image}
+        alt={name}
+        className={styles.image}
+      />
       <div className={styles.details}>
         <div className={styles.name}>{name}</div>
         <div className={styles.info}>{toStartCase(info)}</div>
