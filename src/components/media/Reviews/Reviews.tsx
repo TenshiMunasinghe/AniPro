10c5f7b19ddf3bd2db2c5031988bd72a91ac3bda
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-07-31 22:07:06 +0530

    initial commit

---
+++ src/components/media/Reviews/Reviews.tsx
@@ -0,0 +1,35 @@
+import { useParams } from 'react-router-dom'
+import { useAnimeReviews } from '../../../hooks/useAnimeReviews'
+import { ParamTypes } from '../../../pages/media/Media'
+import LoadMore from '../LoadMore/LoadMore'
+import Review from '../Review/Review'
+import styles from './Reviews.module.scss'
+
+const Reviews = () => {
+  const { id } = useParams<ParamTypes>()
+  const {
+    data,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useAnimeReviews(id)
+
+  if (!data) return null
+
+  return (
+    <div className={styles.container}>
+      {data.pages.map(page =>
+        page.nodes.map(review => (
+          <Review key={'review' + review.id} review={review} />
+        ))
+      )}
+      <LoadMore
+        isFetchingNextPage={isFetchingNextPage}
+        hasNextPage={hasNextPage || false}
+        onClick={() => fetchNextPage()}
+      />
+    </div>
+  )
+}
+
+export default Reviews
