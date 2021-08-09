ca8384253b19dd84706580f50f3929f65df31ac9
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-08-09 17:46:45 +0530

    adjust size

--- src/components/media/Episode./Episode.tsx
+++ src/components/media/Episode./Episode.tsx
@@ -1,5 +1,7 @@
-import { CSSProperties } from 'react'
+import { useContext } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { Watch } from '../../../api/types'
+import { context } from '../../../pages/media/Media'
 import styles from './Episode.module.scss'
 
 interface Props {
@@ -7,9 +9,15 @@ interface Props {
 }
 
 const Episode = ({ episode }: Props) => {
-  const style = { '--thumbnail': `url(${episode.thumbnail})` } as CSSProperties
+  const { scrollPosition } = useContext(context)
   return (
-    <a href={episode.url} className={styles.container} style={style}>
+    <a href={episode.url} className={styles.container}>
+      <LazyLoadImage
+        src={episode.thumbnail}
+        alt={episode.title}
+        scrollPosition={scrollPosition}
+        className={styles.thumbnail}
+      />
       <h6 className={styles.title}>{episode.title}</h6>
     </a>
   )
