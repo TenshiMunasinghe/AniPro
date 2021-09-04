b877a468bc93fb837b12cd53c782a0a384634aeb
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-09-04 22:58:11 +0530

    generated

--- src/generated/index.ts
+++ src/generated/index.ts
@@ -465,11 +465,15 @@ export type CharacterSubmission = {
   submission?: Maybe<Character>;
   /** Submitter for the submission */
   submitter?: Maybe<User>;
+  /** Data Mod assigned to handle the submission */
+  assignee?: Maybe<User>;
   /** Status of the submission */
   status?: Maybe<SubmissionStatus>;
   /** Inner details of submission status */
   notes?: Maybe<Scalars['String']>;
   source?: Maybe<Scalars['String']>;
+  /** Whether the submission is locked */
+  locked?: Maybe<Scalars['Boolean']>;
   createdAt?: Maybe<Scalars['Int']>;
 };
 
@@ -644,6 +648,7 @@ export type InternalPageMediaSubmissionsArgs = {
   mediaId?: Maybe<Scalars['Int']>;
   submissionId?: Maybe<Scalars['Int']>;
   userId?: Maybe<Scalars['Int']>;
+  assigneeId?: Maybe<Scalars['Int']>;
   status?: Maybe<SubmissionStatus>;
   type?: Maybe<MediaType>;
   sort?: Maybe<Array<Maybe<SubmissionSort>>>;
@@ -654,6 +659,7 @@ export type InternalPageMediaSubmissionsArgs = {
 export type InternalPageCharacterSubmissionsArgs = {
   characterId?: Maybe<Scalars['Int']>;
   userId?: Maybe<Scalars['Int']>;
+  assigneeId?: Maybe<Scalars['Int']>;
   status?: Maybe<SubmissionStatus>;
   sort?: Maybe<Array<Maybe<SubmissionSort>>>;
 };
@@ -663,6 +669,7 @@ export type InternalPageCharacterSubmissionsArgs = {
 export type InternalPageStaffSubmissionsArgs = {
   staffId?: Maybe<Scalars['Int']>;
   userId?: Maybe<Scalars['Int']>;
+  assigneeId?: Maybe<Scalars['Int']>;
   status?: Maybe<SubmissionStatus>;
   sort?: Maybe<Array<Maybe<SubmissionSort>>>;
 };
@@ -728,6 +735,7 @@ export type InternalPageMediaArgs = {
   popularity?: Maybe<Scalars['Int']>;
   source?: Maybe<MediaSource>;
   countryOfOrigin?: Maybe<Scalars['CountryCode']>;
+  isLicensed?: Maybe<Scalars['Boolean']>;
   search?: Maybe<Scalars['String']>;
   id_not?: Maybe<Scalars['Int']>;
   id_in?: Maybe<Array<Maybe<Scalars['Int']>>>;
@@ -1288,6 +1296,42 @@ export type MediaCoverImage = {
   color?: Maybe<Scalars['String']>;
 };
 
+/** Notification for when a media entry's data was changed in a significant way impacting users' list tracking */
+export type MediaDataChangeNotification = {
+  __typename?: 'MediaDataChangeNotification';
+  /** The id of the Notification */
+  id: Scalars['Int'];
+  /** The type of notification */
+  type?: Maybe<NotificationType>;
+  /** The id of the media that received data changes */
+  mediaId: Scalars['Int'];
+  /** The reason for the media data change */
+  context?: Maybe<Scalars['String']>;
+  /** The reason for the media data change */
+  reason?: Maybe<Scalars['String']>;
+  /** The time the notification was created at */
+  createdAt?: Maybe<Scalars['Int']>;
+  /** The media that received data changes */
+  media?: Maybe<Media>;
+};
+
+/** Notification for when a media tracked in a user's list is deleted from the site */
+export type MediaDeletionNotification = {
+  __typename?: 'MediaDeletionNotification';
+  /** The id of the Notification */
+  id: Scalars['Int'];
+  /** The type of notification */
+  type?: Maybe<NotificationType>;
+  /** The title of the deleted media */
+  deletedMediaTitle?: Maybe<Scalars['String']>;
+  /** The reason for the media deletion */
+  context?: Maybe<Scalars['String']>;
+  /** The reason for the media deletion */
+  reason?: Maybe<Scalars['String']>;
+  /** The time the notification was created at */
+  createdAt?: Maybe<Scalars['Int']>;
+};
+
 /** Media connection edge */
 export type MediaEdge = {
   __typename?: 'MediaEdge';
@@ -1593,6 +1637,27 @@ export type MediaListTypeOptions = {
   advancedScoringEnabled?: Maybe<Scalars['Boolean']>;
 };
 
+/** Notification for when a media entry is merged into another for a user who had it on their list */
+export type MediaMergeNotification = {
+  __typename?: 'MediaMergeNotification';
+  /** The id of the Notification */
+  id: Scalars['Int'];
+  /** The type of notification */
+  type?: Maybe<NotificationType>;
+  /** The id of the media that was merged into */
+  mediaId: Scalars['Int'];
+  /** The title of the deleted media */
+  deletedMediaTitles?: Maybe<Array<Maybe<Scalars['String']>>>;
+  /** The reason for the media data change */
+  context?: Maybe<Scalars['String']>;
+  /** The reason for the media merge */
+  reason?: Maybe<Scalars['String']>;
+  /** The time the notification was created at */
+  createdAt?: Maybe<Scalars['Int']>;
+  /** The media that was merged into */
+  media?: Maybe<Media>;
+};
+
 /** The ranking of a media in a particular time span and format compared to other media */
 export type MediaRank = {
   __typename?: 'MediaRank';
@@ -1718,12 +1783,24 @@ export enum MediaSource {
   VideoGame = 'VIDEO_GAME',
   /** Other */
   Other = 'OTHER',
-  /** Version 2 only. Written works not published in volumes */
+  /** Version 2+ only. Written works not published in volumes */
   Novel = 'NOVEL',
-  /** Version 2 only. Self-published works */
+  /** Version 2+ only. Self-published works */
   Doujinshi = 'DOUJINSHI',
-  /** Version 2 only. Japanese Anime */
-  Anime = 'ANIME'
+  /** Version 2+ only. Japanese Anime */
+  Anime = 'ANIME',
+  /** Version 3 only. Written works published online */
+  WebNovel = 'WEB_NOVEL',
+  /** Version 3 only. Live action media such as movies or TV show */
+  LiveAction = 'LIVE_ACTION',
+  /** Version 3 only. Games excluding video games */
+  Game = 'GAME',
+  /** Version 3 only. Comics excluding manga */
+  Comic = 'COMIC',
+  /** Version 3 only. Multimedia project */
+  MultimediaProject = 'MULTIMEDIA_PROJECT',
+  /** Version 3 only. Picture book */
+  PictureBook = 'PICTURE_BOOK'
 }
 
 /** A media's statistics */
@@ -1769,12 +1846,16 @@ export type MediaSubmission = {
   id: Scalars['Int'];
   /** User submitter of the submission */
   submitter?: Maybe<User>;
+  /** Data Mod assigned to handle the submission */
+  assignee?: Maybe<User>;
   /** Status of the submission */
   status?: Maybe<SubmissionStatus>;
   submitterStats?: Maybe<Scalars['Json']>;
   notes?: Maybe<Scalars['String']>;
   source?: Maybe<Scalars['String']>;
   changes?: Maybe<Array<Maybe<Scalars['String']>>>;
+  /** Whether the submission is locked */
+  locked?: Maybe<Scalars['Boolean']>;
   media?: Maybe<Media>;
   submission?: Maybe<Media>;
   characters?: Maybe<Array<Maybe<MediaSubmissionComparison>>>;
@@ -1833,6 +1914,8 @@ export type MediaTag = {
   isMediaSpoiler?: Maybe<Scalars['Boolean']>;
   /** If the tag is only for adult 18+ media */
   isAdult?: Maybe<Scalars['Boolean']>;
+  /** The user who submitted the tag */
+  userId?: Maybe<Scalars['Int']>;
 };
 
 /** The official titles of the media in various languages */
@@ -2385,11 +2468,17 @@ export enum NotificationType {
   /** A user has replied to activity you have also replied to */
   ActivityReplySubscribed = 'ACTIVITY_REPLY_SUBSCRIBED',
   /** A new anime or manga has been added to the site where its related media is on the user's list */
-  RelatedMediaAddition = 'RELATED_MEDIA_ADDITION'
+  RelatedMediaAddition = 'RELATED_MEDIA_ADDITION',
+  /** An anime or manga has had a data change that affects how a user may track it in their lists */
+  MediaDataChange = 'MEDIA_DATA_CHANGE',
+  /** Anime or manga entries on the user's list have been merged into a single entry */
+  MediaMerge = 'MEDIA_MERGE',
+  /** An anime or manga on the user's list has been deleted from the site */
+  MediaDeletion = 'MEDIA_DELETION'
 }
 
 /** Notification union type */
-export type NotificationUnion = AiringNotification | FollowingNotification | ActivityMessageNotification | ActivityMentionNotification | ActivityReplyNotification | ActivityReplySubscribedNotification | ActivityLikeNotification | ActivityReplyLikeNotification | ThreadCommentMentionNotification | ThreadCommentReplyNotification | ThreadCommentSubscribedNotification | ThreadCommentLikeNotification | ThreadLikeNotification | RelatedMediaAdditionNotification;
+export type NotificationUnion = AiringNotification | FollowingNotification | ActivityMessageNotification | ActivityMentionNotification | ActivityReplyNotification | ActivityReplySubscribedNotification | ActivityLikeNotification | ActivityReplyLikeNotification | ThreadCommentMentionNotification | ThreadCommentReplyNotification | ThreadCommentSubscribedNotification | ThreadCommentLikeNotification | ThreadLikeNotification | RelatedMediaAdditionNotification | MediaDataChangeNotification | MediaMergeNotification | MediaDeletionNotification;
 
 /** Page of data */
 export type Page = {
@@ -2453,6 +2542,7 @@ export type PageMediaArgs = {
   popularity?: Maybe<Scalars['Int']>;
   source?: Maybe<MediaSource>;
   countryOfOrigin?: Maybe<Scalars['CountryCode']>;
+  isLicensed?: Maybe<Scalars['Boolean']>;
   search?: Maybe<Scalars['String']>;
   id_not?: Maybe<Scalars['Int']>;
   id_in?: Maybe<Array<Maybe<Scalars['Int']>>>;
@@ -2804,8 +2894,6 @@ export type Query = {
   AniChartUser?: Maybe<AniChartUser>;
   /** Site statistics query */
   SiteStatistics?: Maybe<SiteStatistics>;
-  /** Get the user who added a tag to a media */
-  MediaTagUser?: Maybe<User>;
 };
 
 
@@ -2840,6 +2928,7 @@ export type QueryMediaArgs = {
   popularity?: Maybe<Scalars['Int']>;
   source?: Maybe<MediaSource>;
   countryOfOrigin?: Maybe<Scalars['CountryCode']>;
+  isLicensed?: Maybe<Scalars['Boolean']>;
   search?: Maybe<Scalars['String']>;
   id_not?: Maybe<Scalars['Int']>;
   id_in?: Maybe<Array<Maybe<Scalars['Int']>>>;
@@ -3144,12 +3233,6 @@ export type QueryLikeArgs = {
 
 export type QueryMarkdownArgs = {
   markdown: Scalars['String'];
-};
-
-
-export type QueryMediaTagUserArgs = {
-  tagId?: Maybe<Scalars['Int']>;
-  mediaId?: Maybe<Scalars['Int']>;
 };
 
 /** Media recommendation */
@@ -3674,11 +3757,15 @@ export type StaffSubmission = {
   submission?: Maybe<Staff>;
   /** Submitter for the submission */
   submitter?: Maybe<User>;
+  /** Data Mod assigned to handle the submission */
+  assignee?: Maybe<User>;
   /** Status of the submission */
   status?: Maybe<SubmissionStatus>;
   /** Inner details of submission status */
   notes?: Maybe<Scalars['String']>;
   source?: Maybe<Scalars['String']>;
+  /** Whether the submission is locked */
+  locked?: Maybe<Scalars['Boolean']>;
   createdAt?: Maybe<Scalars['Int']>;
 };
 
@@ -4515,7 +4602,7 @@ export type CommonQueryVariables = Exact<{
 }>;
 
 
-export type CommonQuery = { __typename?: 'Query', Media?: Maybe<{ __typename?: 'Media', bannerImage?: Maybe<string>, description?: Maybe<string>, format?: Maybe<MediaFormat>, episodes?: Maybe<number>, duration?: Maybe<number>, status?: Maybe<MediaStatus>, season?: Maybe<MediaSeason>, seasonYear?: Maybe<number>, averageScore?: Maybe<number>, meanScore?: Maybe<number>, popularity?: Maybe<number>, favourites?: Maybe<number>, source?: Maybe<MediaSource>, hashtag?: Maybe<string>, genres?: Maybe<Array<Maybe<string>>>, synonyms?: Maybe<Array<Maybe<string>>>, title?: Maybe<{ __typename?: 'MediaTitle', romaji?: Maybe<string>, english?: Maybe<string>, native?: Maybe<string> }>, coverImage?: Maybe<{ __typename?: 'MediaCoverImage', large?: Maybe<string>, extraLarge?: Maybe<string>, color?: Maybe<string> }>, nextAiringEpisode?: Maybe<{ __typename?: 'AiringSchedule', episode: number, timeUntilAiring: number }>, startDate?: Maybe<{ __typename?: 'FuzzyDate', year?: Maybe<number>, month?: Maybe<number>, day?: Maybe<number> }>, endDate?: Maybe<{ __typename?: 'FuzzyDate', year?: Maybe<number>, month?: Maybe<number>, day?: Maybe<number> }>, studios?: Maybe<{ __typename?: 'StudioConnection', nodes?: Maybe<Array<Maybe<{ __typename?: 'Studio', name: string }>>> }>, externalLinks?: Maybe<Array<Maybe<{ __typename?: 'MediaExternalLink', url: string, site: string }>>>, characters?: Maybe<{ __typename?: 'CharacterConnection', edges?: Maybe<Array<Maybe<{ __typename?: 'CharacterEdge', node?: Maybe<{ __typename?: 'Character', id: number }> }>>> }>, staff?: Maybe<{ __typename?: 'StaffConnection', edges?: Maybe<Array<Maybe<{ __typename?: 'StaffEdge', node?: Maybe<{ __typename?: 'Staff', id: number }> }>>> }>, streamingEpisodes?: Maybe<Array<Maybe<{ __typename?: 'MediaStreamingEpisode', title?: Maybe<string>, url?: Maybe<string> }>>> }> };
+export type CommonQuery = { __typename?: 'Query', Media?: Maybe<{ __typename?: 'Media', bannerImage?: Maybe<string>, description?: Maybe<string>, format?: Maybe<MediaFormat>, episodes?: Maybe<number>, duration?: Maybe<number>, status?: Maybe<MediaStatus>, season?: Maybe<MediaSeason>, seasonYear?: Maybe<number>, averageScore?: Maybe<number>, meanScore?: Maybe<number>, popularity?: Maybe<number>, favourites?: Maybe<number>, source?: Maybe<MediaSource>, hashtag?: Maybe<string>, genres?: Maybe<Array<Maybe<string>>>, synonyms?: Maybe<Array<Maybe<string>>>, title?: Maybe<{ __typename?: 'MediaTitle', romaji?: Maybe<string>, english?: Maybe<string>, native?: Maybe<string> }>, coverImage?: Maybe<{ __typename?: 'MediaCoverImage', large?: Maybe<string>, extraLarge?: Maybe<string>, color?: Maybe<string> }>, nextAiringEpisode?: Maybe<{ __typename?: 'AiringSchedule', episode: number, timeUntilAiring: number }>, startDate?: Maybe<{ __typename?: 'FuzzyDate', year?: Maybe<number>, month?: Maybe<number>, day?: Maybe<number> }>, endDate?: Maybe<{ __typename?: 'FuzzyDate', year?: Maybe<number>, month?: Maybe<number>, day?: Maybe<number> }>, studios?: Maybe<{ __typename?: 'StudioConnection', nodes?: Maybe<Array<Maybe<{ __typename?: 'Studio', name: string }>>> }>, externalLinks?: Maybe<Array<Maybe<{ __typename?: 'MediaExternalLink', url: string, site: string }>>>, characters?: Maybe<{ __typename?: 'CharacterConnection', edges?: Maybe<Array<Maybe<{ __typename?: 'CharacterEdge', node?: Maybe<{ __typename?: 'Character', id: number }> }>>> }>, staff?: Maybe<{ __typename?: 'StaffConnection', edges?: Maybe<Array<Maybe<{ __typename?: 'StaffEdge', node?: Maybe<{ __typename?: 'Staff', id: number }> }>>> }>, streamingEpisodes?: Maybe<Array<Maybe<{ __typename?: 'MediaStreamingEpisode', title?: Maybe<string>, url?: Maybe<string> }>>>, reviews?: Maybe<{ __typename?: 'ReviewConnection', nodes?: Maybe<Array<Maybe<{ __typename?: 'Review', id: number }>>> }> }> };
 
 export type OverviewQueryVariables = Exact<{
   id: Scalars['Int'];
@@ -4692,6 +4779,11 @@ export const CommonDocument = `
     streamingEpisodes {
       title
       url
+    }
+    reviews(perPage: 1) {
+      nodes {
+        id
+      }
     }
   }
 }
