7ed3be0dc474e301e78e3467de053237b3dcda1a
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-09-04 22:30:09 +0530

    make centering of the spinner optional

--- src/components/common/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -1,7 +1,23 @@
+import classnames from 'classnames'
 import styles from './LoadingSpinner.module.scss'
 
-const LoadingSpinner = () => {
-  return <div className={styles.container}>A</div>
+interface Props {
+  isCenter?: {
+    x: boolean
+    y: boolean
+  }
+}
+
+const LoadingSpinner = ({ isCenter = { x: true, y: true } }: Props) => {
+  return (
+    <div
+      className={classnames(styles.container, {
+        [styles.centerX]: isCenter.x,
+        [styles.centerY]: isCenter.y,
+      })}>
+      A
+    </div>
+  )
 }
 
 export default LoadingSpinner
