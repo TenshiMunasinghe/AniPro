d0756b900061993c805e3b56ad6bf8156a11e509
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-09-06 19:27:45 +0530

    add hover effect on image

--- src/components/media/Episode./Episode.tsx
+++ src/components/media/Episode./Episode.tsx
@@ -13,12 +13,15 @@ const Episode = ({ episode }: Props) => {
   const { scrollPosition } = useContext(context)
   return (
     <a href={episode?.url || ''} className={styles.container}>
-      <LazyLoadImage
-        src={episode?.thumbnail || NO_IMAGE_URL}
-        alt={episode?.title || 'no image'}
-        scrollPosition={scrollPosition}
-        className={styles.thumbnail}
-      />
+      <figure className={styles.imageContainer}>
+        <LazyLoadImage
+          src={episode?.thumbnail || NO_IMAGE_URL}
+          alt={episode?.title || 'no image'}
+          scrollPosition={scrollPosition}
+          className={styles.thumbnail}
+        />
+      </figure>
+
       <h6 className={styles.title}>{episode?.title || 'no title'}</h6>
     </a>
   )
