88a95fdbfbfabed97798eff3621de4ee6bf81dce
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-09-15 18:52:30 +0530

    include rankings section

--- src/components/media/Aside/Aside.tsx
+++ src/components/media/Aside/Aside.tsx
@@ -5,6 +5,7 @@ import { formatLabel } from '../../../utils/formatLabel'
 import { timeToArr } from '../../../utils/timeToArr'
 import { timeToStr } from '../../../utils/timeToStr'
 import { toStartCase } from '../../../utils/toStartCase'
+import Ranking from '../Ranking/Ranking'
 import styles from './Aside.module.scss'
 import Item from './Item/Item'
 
@@ -23,88 +24,99 @@ const Aside = ({ data }: Props) => {
 
   return (
     <aside className={styles.container}>
-      <Item label='Airing'>
-        {data.nextAiringEpisode
-          ? airingInfo({
-              nextAiringEpisode: data.nextAiringEpisode,
-              season: null,
-              seasonYear: null,
-            })
-          : ''}
-      </Item>
+      {data.rankings?.length && (
+        <section className={styles.rankings}>
+          {data.rankings
+            .filter(ranking => ranking?.allTime)
+            .map(ranking => (
+              <Ranking ranking={ranking} />
+            ))}
+        </section>
+      )}
+      <section className={styles.infos}>
+        <Item label='Airing'>
+          {data.nextAiringEpisode
+            ? airingInfo({
+                nextAiringEpisode: data.nextAiringEpisode,
+                season: null,
+                seasonYear: null,
+              })
+            : ''}
+        </Item>
 
-      <Item label='Format'>{formatLabel(data.format || '')}</Item>
+        <Item label='Format'>{formatLabel(data.format || '')}</Item>
 
-      <Item label='Episodes'>{data.episodes?.toString() || ''}</Item>
+        <Item label='Episodes'>{data.episodes?.toString() || ''}</Item>
 
-      <Item label='Episode Duration'>
-        {data.duration
-          ? timeToStr(
-              timeToArr(
-                convertTime({
-                  num: data.duration,
-                  input: 'minutes',
-                  output:
-                    data.format === 'MOVIE'
-                      ? ['hours', 'minutes']
-                      : ['minutes'],
-                })
+        <Item label='Episode Duration'>
+          {data.duration
+            ? timeToStr(
+                timeToArr(
+                  convertTime({
+                    num: data.duration,
+                    input: 'minutes',
+                    output:
+                      data.format === 'MOVIE'
+                        ? ['hours', 'minutes']
+                        : ['minutes'],
+                  })
+                )
               )
-            )
-          : ''}
-      </Item>
+            : ''}
+        </Item>
 
-      <Item label='Status'>{toStartCase(data.status || '')}</Item>
+        <Item label='Status'>{toStartCase(data.status || '')}</Item>
 
-      <Item label='Start Date'>
-        {data.startDate?.year
-          ? new Date(
-              data.startDate.year,
-              data.startDate.month || 0 - 1,
-              data.startDate.day || 0
-            ).toLocaleDateString('en-US', dateFormat)
-          : ''}
-      </Item>
+        <Item label='Start Date'>
+          {data.startDate?.year
+            ? new Date(
+                data.startDate.year,
+                data.startDate.month || 0 - 1,
+                data.startDate.day || 0
+              ).toLocaleDateString('en-US', dateFormat)
+            : ''}
+        </Item>
 
-      <Item label='End Date'>
-        {data.endDate?.year
-          ? new Date(
-              data.endDate.year,
-              data.endDate?.month || 0 - 1,
-              data.endDate?.day || 0
-            ).toLocaleDateString('en-US', dateFormat)
-          : ''}
-      </Item>
+        <Item label='End Date'>
+          {data.endDate?.year
+            ? new Date(
+                data.endDate.year,
+                data.endDate?.month || 0 - 1,
+                data.endDate?.day || 0
+              ).toLocaleDateString('en-US', dateFormat)
+            : ''}
+        </Item>
 
-      <Item label='Season'>
-        {data.season && data.seasonYear
-          ? `${toStartCase(data.season)} ${data.seasonYear.toString()}`
-          : ''}
-      </Item>
+        <Item label='Season'>
+          {data.season && data.seasonYear
+            ? `${toStartCase(data.season)} ${data.seasonYear.toString()}`
+            : ''}
+        </Item>
 
-      <Item label='Average Score'>
-        {data.meanScore ? `${data.meanScore.toString()}%` : ''}
-      </Item>
+        <Item label='Average Score'>
+          {data.meanScore ? `${data.meanScore.toString()}%` : ''}
+        </Item>
 
-      <Item label='Popularity'>{data.popularity?.toString() || ''}</Item>
+        <Item label='Popularity'>{data.popularity?.toString() || ''}</Item>
 
-      <Item label='Favorites'>{data.favourites?.toString() || ''}</Item>
+        <Item label='Favorites'>{data.favourites?.toString() || ''}</Item>
 
-      <Item label='Studios'>{data.studios?.nodes?.[0]?.name || ''}</Item>
+        <Item label='Studios'>{data.studios?.nodes?.[0]?.name || ''}</Item>
 
-      <Item label='Source'>{toStartCase(data.source || '')}</Item>
+        <Item label='Source'>{toStartCase(data.source || '')}</Item>
 
-      <Item label='Hashtag'>{data.hashtag || ''}</Item>
+        <Item label='Hashtag'>{data.hashtag || ''}</Item>
 
-      <Item label='Genres'>{data.genres?.join(', ') || ''}</Item>
+        <Item label='Genres'>{data.genres?.join(', ') || ''}</Item>
 
-      <Item label='Romaji'>{data.title?.romaji || ''}</Item>
+        <Item label='Romaji'>{data.title?.romaji || ''}</Item>
 
-      <Item label='English'>{data.title?.english || ''}</Item>
+        <Item label='English'>{data.title?.english || ''}</Item>
 
-      <Item label='Native'>{data.title?.native || ''}</Item>
+        <Item label='Native'>{data.title?.native || ''}</Item>
 
-      <Item label='Synonyms'>{data.synonyms?.join(' ') || ''}</Item>
+        <Item label='Synonyms'>{data.synonyms?.join(' ') || ''}</Item>
+      </section>
     </aside>
   )
 }
