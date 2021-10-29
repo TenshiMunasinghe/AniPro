21289f779299d36546a0a06f4af4f1377328a134
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-10-29 09:22:59 +0530

    add search bar

--- src/components/search/Media/Media.tsx
+++ src/components/search/Media/Media.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { MediaType } from '../../../generated/index'
 import { useUpdateUrlParam } from '../../../hooks/useUpdateUrlParam'
+import SearchBarInput from '../../common/SearchBarInput/SearchBarInput'
 import Home from './Home'
+import styles from './Home.module.scss'
 import MediaSearchResult from './MediaSearchResult/MediaSearchResult'
 
 interface Props {
@@ -12,7 +14,8 @@ const Media = ({ type }: Props) => {
   const { queryVars } = useUpdateUrlParam()
 
   return (
-    <div>
+    <div className={styles.container}>
+      <SearchBarInput link={`/search/${type.toLowerCase()}`} />
       {Object.keys(queryVars.initial).length === 0 ? (
         <Home type={type} />
       ) : (
