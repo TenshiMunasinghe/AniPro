53542f31e4200b1cf239fd85b770cdbc6e9d0c3d
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-11-09 13:13:24 +0530

    implement dynamic positioning of icon

--- src/components/common/Dropdown/Dropdown.tsx
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames'
 import { memo, RefObject } from 'react'
+import { IconType } from 'react-icons'
 import { FaAngleDown } from 'react-icons/fa'
 import { useFocusedWithin } from '../../../hooks/useFocusedWithin'
 import styles from './Dropdown.module.scss'
@@ -14,6 +15,7 @@ interface Props {
   }[]
   selected: string | (string | null)[]
   placeholder?: string
+  icon?: { type: IconType; isLeft?: boolean }
 }
 const Dropdown = ({
   onChange,
@@ -21,6 +23,7 @@ const Dropdown = ({
   options,
   selected,
   placeholder,
+  icon,
 }: Props) => {
   const { ref, isFocused } = useFocusedWithin()
 
@@ -42,6 +45,8 @@ const Dropdown = ({
 
   const selectedLabel = options.find(o => o.value === selected)?.label
 
+  const Icon = () => (icon ? <icon.type /> : <FaAngleDown />)
+
   return (
     <div
       className={styles.wrapper}
@@ -50,13 +55,14 @@ const Dropdown = ({
       ref={ref as RefObject<HTMLDivElement>}
       tabIndex={0}>
       <button className={styles.dropdownHeader} tabIndex={-1}>
+        {icon?.isLeft && <Icon aria-label='toggle dropdown' />}
         <div
           className={classnames(styles.selected, {
             [styles.empty]: !selectedLabel,
           })}>
           {selectedLabel || placeholder || ''}
         </div>
-        <FaAngleDown aria-label='toggle dropdown' />
+        {!icon?.isLeft && <Icon aria-label='toggle dropdown' />}
       </button>
       <Items
         isVisible={isFocused}
