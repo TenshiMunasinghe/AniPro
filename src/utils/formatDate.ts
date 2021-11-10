7f6b8378ed7127a74612189afd845efedc6dcfe6
TenshiMunasinghe <shadowghost1007@gmail.com>
2021-11-10 13:51:43 +0530

    add util to format date

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,33 @@
+const dateFormat = {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+}
+
+export const formatDate = ({
+  year,
+  month,
+  day,
+}: {
+  year?: number | null
+  month?: number | null
+  day?: number | null
+}) => {
+  const string = new Date(year || 0, month || -1, day || 0)
+    .toLocaleDateString('en-US', dateFormat)
+    .replaceAll(',', '')
+
+  const arr = string.split(' ')
+
+  if (!day) {
+    arr.splice(0, 1)
+  }
+  if (!month) {
+    arr.splice(1, 1)
+  }
+  if (!year) {
+    arr.splice(2, 1)
+  }
+
+  return arr.join(' ')
+}
