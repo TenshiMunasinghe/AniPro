5b46a03b7adc96850823b84510ca3bfbd7ccdd6f
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-01-07 10:53:59 +0530

    allow swtiching between character and staff roles

--- src/components/staff/Medias/Medias.tsx
+++ src/components/staff/Medias/Medias.tsx
@@ -1,10 +1,43 @@
+import classnames from 'classnames'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import gqlRequestClient from '../../../api/graphqlClient'
+import { useStaffRoleCountsQuery } from '../../../generated/index'
+import LoadingSpinner from '../../common/LoadingSpinner/LoadingSpinner'
 import Characters from './Characters/Characters'
 import styles from './Medias.module.scss'
+import StaffRoles from './StaffRoles/StaffRoles'
 
 const Medias = () => {
+  const { id } = useParams<{ id: string }>()
+  const { data, isLoading } = useStaffRoleCountsQuery(gqlRequestClient, {
+    id: parseInt(id),
+  })
+  const characterCount = data?.Staff?.characterMedia?.pageInfo?.total
+  const staffCount = data?.Staff?.staffMedia?.pageInfo?.total
+
+  const [role, setRole] = useState<'voiceActor' | 'staff'>(
+    (characterCount || 0) >= (staffCount || 0) ? 'voiceActor' : 'staff'
+  )
+
+  if (isLoading) return <LoadingSpinner />
+
   return (
     <div className={styles.container}>
-      <Characters />
+      {characterCount && staffCount && (
+        <div
+          className={classnames(styles.roles, {
+            [styles.vaActive]: role === 'voiceActor',
+            [styles.staffActive]: role === 'staff',
+          })}>
+          <button onClick={() => setRole('voiceActor')}>
+            Voice Acting Roles
+          </button>
+          <button onClick={() => setRole('staff')}>Staff Roles</button>
+        </div>
+      )}
+      {role === 'voiceActor' && <Characters />}
+      {role === 'staff' && <StaffRoles />}
     </div>
   )
 }
---
+++ src/components/staff/Medias/StaffRoles/StaffRoles.tsx
@@ -0,0 +1,9 @@
+import styles from './StaffRoles.module.scss'
+
+interface Props {}
+
+const Roles = (props: Props) => {
+  return <div className={styles.container}></div>
+}
+
+export default Roles
