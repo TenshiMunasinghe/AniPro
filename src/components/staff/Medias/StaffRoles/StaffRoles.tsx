1cee410d424f4ca230655aea6e9470fda19e4e4f
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-01-14 12:31:09 +0530

    get pageInfo

--- src/components/staff/Medias/StaffRoles/StaffRoles.tsx
+++ src/components/staff/Medias/StaffRoles/StaffRoles.tsx
@@ -1,9 +1,112 @@
+import classnames from 'classnames'
+import { groupBy } from 'lodash'
+import React from 'react'
+import { FaSort } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
+import { MediaSort, useStaffMediaRoleQuery } from '../../../../generated/index'
+import { useInfiniteGraphQLQuery } from '../../../../hooks/useInfiniteGraphQLQuery'
+import { useSortMedia } from '../../../../hooks/useSortMedia'
+import Dropdown from '../../../common/Dropdown/Dropdown'
+import LoadingSpinner from '../../../common/LoadingSpinner/LoadingSpinner'
+import LoadMore from '../../../common/LoadMore/LoadMore'
+import { sortByOptions } from '../../../search/Media/MediaSearchResult/MediaSearchResult'
+import Cards from '../Characters/Cards/Cards'
 import styles from './StaffRoles.module.scss'
 
 interface Props {}
 
 const Roles = (props: Props) => {
-  return <div className={styles.container}></div>
+  const { id } = useParams<{ id: string }>()
+
+  const { sortBy, sortByOnChange } = useSortMedia()
+
+  const {
+    data,
+    isLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteGraphQLQuery(
+    useStaffMediaRoleQuery,
+    ({ pageParam = 1 }) => ({
+      id: parseInt(id),
+      page: pageParam,
+      sort: sortBy,
+    }),
+    {
+      getNextPageParam: ({ Staff }) => {
+        if (!Staff?.staffMedia?.pageInfo?.currentPage) return
+
+        const {
+          staffMedia: { pageInfo },
+        } = Staff
+
+        return pageInfo.hasNextPage
+          ? (pageInfo?.currentPage || 0) + 1
+          : undefined
+      },
+    }
+  )
+
+  const edges = data?.pages.flatMap(page => page.Staff?.staffMedia?.edges)
+
+  const groupedEdges = groupBy(
+    edges,
+    edge => edge?.node?.startDate?.year || 'TBA'
+  )
+
+  const { TBA, ...edgesWithYears } = groupedEdges
+
+  const Tba = () =>
+    TBA?.length ? (
+      <div>
+        <h6 className={styles.year}>TBA</h6>
+        <Cards edges={TBA} />
+      </div>
+    ) : null
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.dropdowns}>
+        <Dropdown
+          selected={sortBy}
+          onChange={sortByOnChange}
+          options={sortByOptions}
+          icon={{ type: FaSort, isLeft: true }}
+        />
+      </div>
+      <div className={classnames(styles.cardContainer)}>
+        {isLoading && <LoadingSpinner />}
+
+        {!isLoading &&
+          ([MediaSort.StartDate, MediaSort.StartDateDesc].includes(sortBy) ? (
+            <>
+              {sortBy === MediaSort.StartDateDesc && <Tba />}
+              {Object.entries(edgesWithYears)
+                .sort(([a], [b]) =>
+                  sortBy === MediaSort.StartDateDesc
+                    ? parseInt(b) - parseInt(a)
+                    : parseInt(a) - parseInt(b)
+                )
+                .map(([year, edges]) => (
+                  <div key={String(year) + String(id)}>
+                    <h6 className={styles.year}>{year}</h6>
+                    <Cards edges={edges} />
+                  </div>
+                ))}
+              {sortBy === MediaSort.StartDate && <Tba />}
+            </>
+          ) : (
+            <Cards edges={edges} />
+          ))}
+      </div>
+      <LoadMore
+        isFetchingNextPage={isFetchingNextPage}
+        hasNextPage={hasNextPage || false}
+        onClick={() => fetchNextPage()}
+      />
+    </div>
+  )
 }
 
 export default Roles
