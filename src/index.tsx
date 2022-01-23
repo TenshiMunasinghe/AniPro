ef6d8dc1fc8694c5fab765d17018437b82be496d
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-01-23 17:25:35 +0530

    render app into a div

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ ReactDOM.render(
       <App />
     </div>
   </StrictMode>,
-  document.body
+  document.getElementById('anipro')
 )
 
 // If you want your app to work offline and load faster, you can change
