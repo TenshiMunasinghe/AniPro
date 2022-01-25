0d4d444f8ce072b283b03d7de1bfa5a9352bc9d5
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-01-25 21:10:02 +0530

    remove unused object

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,20 +1,5 @@
 import { Link } from 'react-router-dom'
 
-const sizes = {
-  sm: 'text-sm',
-  md: 'text-base',
-  lg: 'text-lg',
-  xl: 'text-xl',
-  '2xl': 'text-2xl',
-  '3xl': 'text-3xl',
-  '4xl': 'text-4xl',
-  '5xl': 'text-5xl',
-  '6xl': 'text-6xl',
-  '7xl': 'text-7xl',
-  '8xl': 'text-8xl',
-  '9xl': 'text-9xl',
-}
-
 interface Props {
   link?: string | null
   text: string
