f3a6bdf0f478488f6b051fa2594d41f552b3b291
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-01-31 20:14:39 +0530

    adust shadow color

--- src/components/common/Cards/CardCover/Content/Content.tsx
+++ src/components/common/Cards/CardCover/Content/Content.tsx
@@ -19,7 +19,7 @@ const Content = ({ main, sub, rank }: Props) => {
           <Rank rank={rank} />
         </div>
       )}
-      <div className='relative h-fit rounded overflow-hidden shadow shadow-zinc-300 dark:shadow-zinc-900'>
+      <div className='relative h-fit rounded overflow-hidden shadow shadow-zinc-700 dark:shadow-zinc-900'>
         <figure className='grid'>
           <CoverImage
             link={main.link}
