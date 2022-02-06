5c2167771fc473a0914552dedaa637462a3507e4
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-02-06 19:06:57 +0530

    implement scroll snap and better keyboard support

--- src/components/home/Slide.tsx
+++ src/components/home/Slide.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, memo, useCallback } from 'react'
+import { forwardRef, memo, useEffect, useMemo, useRef } from 'react'
 import { DeepPartial } from 'react-hook-form'
 import { NO_IMAGE_URL } from '../../api/queries'
 import { linkToMediaPage } from '../../App'
@@ -12,63 +12,62 @@ import Title from '../common/Title'
 
 interface Props {
   media?: DeepPartial<Media> | null
-  index: number
-  setSlide: (slide: number) => void
 }
 
-const Slide = forwardRef<HTMLDivElement[], Props>(
-  ({ media, setSlide, index }, ref) => {
-    const colors = useColorVariable(media?.coverImage?.color)
+const Slide = forwardRef<HTMLElement, Props>(({ media }, slideRef) => {
+  const colors = useColorVariable(media?.coverImage?.color)
+  const ref = useRef<HTMLDivElement | null>(null)
 
-    const onFocus = useCallback(
-      e => {
-        if (!e.currentTarget.contains(e.relatedTarget?.parentNode))
-          setSlide(index)
+  const observer = useMemo(() => {
+    return new IntersectionObserver(
+      entries => {
+        if (!entries[0].isIntersecting) return
+        ref.current?.focus()
       },
-      [setSlide, index]
+      {
+        root: typeof slideRef !== 'function' ? slideRef?.current : null,
+        threshold: 1,
+      }
     )
+  }, [slideRef])
 
-    if (!media?.id || !ref) return null
+  useEffect(() => {
+    ref.current && observer.observe(ref.current)
+  }, [observer])
 
-    return (
-      <div
-        className={
-          'relative flex h-screen w-full shrink-0 flex-col justify-center p-4 sm:p-6 lg:px-20'
-        }
-        style={colors}
-        ref={el =>
-          typeof ref !== 'function' && el && ref.current
-            ? (ref.current[index] = el)
-            : null
-        }
-        tabIndex={0}
-        onFocus={onFocus}>
-        <BackgroundImage src={media?.coverImage?.extraLarge} blur='blur-md' />
-        <div className='z-10'>
-          <div className='mb-6 grid justify-between gap-x-10 overflow-y-hidden md:grid-cols-[1fr_20rem]'>
-            <div className='space-y-6'>
-              <Title
-                link={linkToMediaPage(media.id, media.type || MediaType.Anime)}
-                text={media?.title?.romaji || 'no title'}
-                className='text-2xl font-bold text-zinc-900 dark:text-zinc-50 lg:text-6xl lg:leading-tight'
-              />
-              <div className='line-clamp-8 md:text-lg lg:line-clamp-10'>
-                <Description description={media.description} />
-              </div>
-            </div>
-            <div className='hidden overflow-hidden rounded shadow shadow-zinc-300 dark:shadow-zinc-900 lg:grid'>
-              <CoverImage
-                link={linkToMediaPage(media.id, MediaType.Anime)}
-                title={media?.title?.romaji || 'no title'}
-                src={media?.coverImage?.extraLarge || NO_IMAGE_URL}
-              />
+  if (!media?.id) return null
+
+  return (
+    <div
+      className='relative flex h-screen w-full shrink-0 snap-start flex-col justify-center p-4 sm:p-6 lg:px-20'
+      style={colors}
+      tabIndex={0}
+      ref={ref}>
+      <BackgroundImage src={media?.coverImage?.extraLarge} blur='blur-md' />
+      <div className='z-10'>
+        <div className='mb-6 grid justify-between gap-x-10 overflow-y-hidden md:grid-cols-[1fr_20rem]'>
+          <div className='space-y-6'>
+            <Title
+              link={linkToMediaPage(media.id, media.type || MediaType.Anime)}
+              text={media?.title?.romaji || 'no title'}
+              className='text-2xl font-bold text-zinc-900 dark:text-zinc-50 lg:text-6xl lg:leading-tight'
+            />
+            <div className='line-clamp-8 md:text-lg lg:line-clamp-10'>
+              <Description description={media.description} />
             </div>
           </div>
-          <Genres as='section' genres={media.genres} canInteract />
+          <div className='hidden overflow-hidden rounded shadow shadow-zinc-300 dark:shadow-zinc-900 lg:grid'>
+            <CoverImage
+              link={linkToMediaPage(media.id, MediaType.Anime)}
+              title={media?.title?.romaji || 'no title'}
+              src={media?.coverImage?.extraLarge || NO_IMAGE_URL}
+            />
+          </div>
         </div>
+        <Genres as='section' genres={media.genres} canInteract />
       </div>
-    )
-  }
-)
+    </div>
+  )
+})
 
 export default memo(Slide)
--- src/components/home/Slider.tsx
+++ src/components/home/Slider.tsx
@@ -1,6 +1,4 @@
-import classnames from 'classnames'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { FaCaretLeft, FaCaretRight } from 'react-icons/fa'
+import { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import gqlRequestClient from '../../api/graphqlClient'
 import {
@@ -22,53 +20,19 @@ const Slider = ({ queryVar, context }: Props) => {
     perPage: 5,
   })
 
-  const slideCount = data?.Page?.media?.length || 0
-
-  const [slide, setSlide] = useState(0)
-
-  const slideRefs = useRef<HTMLDivElement[]>([])
-
-  useEffect(() => {
-    if (slideRefs.current.length > 0) {
-      slideRefs.current[slide].scrollIntoView({ behavior: 'smooth' })
-      slideRefs.current[slide].focus({ preventScroll: true })
-    }
-  }, [slide])
+  const ref = useRef<HTMLElement | null>(null)
 
   const link = linkToSearchPage(queryVar)
 
-  const toNextSlide = useCallback(
-    () => setSlide(prev => (prev + 1 >= slideCount ? prev : prev + 1)),
-    [slideCount]
-  )
-
-  const toPrevSlide = useCallback(
-    () => setSlide(prev => (prev - 1 < 0 ? prev : prev - 1)),
-    []
-  )
-
-  const onKeyPress = useCallback(
-    e => {
-      if (e.keyCode === 39) {
-        toNextSlide()
-      } else if (e.keyCode === 37) {
-        toPrevSlide()
-      }
-    },
-    [toNextSlide, toPrevSlide]
-  )
-
-  const buttonStyle = {
-    disabled: 'cursor-default opacity-30',
-    active:
-      'text-zinc-500 dark:text-zinc-400 hocus:text-zinc-900 dark:hocus:text-zinc-300',
-  }
+  useEffect(() => {
+    ref.current?.focus()
+  }, [])
 
   return (
     <section
       className='relative flex h-screen min-w-full items-center'
       tabIndex={-1}
-      onKeyDown={onKeyPress}>
+      ref={ref}>
       {isLoading && <LoadingSpinner />}
       {!isLoading && (
         <>
@@ -85,16 +49,10 @@ const Slider = ({ queryVar, context }: Props) => {
             </Link>
           </header>
 
-          {slideCount && (
-            <section className='z-10 flex overflow-hidden'>
-              {data?.Page?.media?.map((media, idx) => (
-                <Slide
-                  media={media}
-                  key={media?.id}
-                  index={idx}
-                  setSlide={setSlide}
-                  ref={slideRefs}
-                />
+          {!!data?.Page?.media?.length && (
+            <section className='z-10 flex snap-x snap-mandatory overflow-x-auto'>
+              {data?.Page?.media?.map(media => (
+                <Slide media={media} ref={ref} key={media?.id} />
               ))}
             </section>
           )}
@@ -102,25 +60,6 @@ const Slider = ({ queryVar, context }: Props) => {
           <footer className='absolute right-4 bottom-4 z-20 hocus:text-teal-600 dark:text-zinc-200 hocus:dark:text-teal-300 sm:right-6 sm:bottom-6 lg:bottom-10 lg:right-20'>
             <Link to={link}>View All</Link>
           </footer>
-
-          <div className='md:-tranzinc-x-1/2 absolute left-4 bottom-4 z-10 sm:bottom-6 md:left-1/2'>
-            <button
-              onClick={toPrevSlide}
-              className={classnames({
-                [buttonStyle.disabled]: slide === 0,
-                [buttonStyle.active]: slide !== 0,
-              })}>
-              <FaCaretLeft size='3em' />
-            </button>
-            <button
-              onClick={toNextSlide}
-              className={classnames({
-                [buttonStyle.disabled]: slide === slideCount - 1,
-                [buttonStyle.active]: slide !== slideCount - 1,
-              })}>
-              <FaCaretRight size='3em' />
-            </button>
-          </div>
         </>
       )}
     </section>
