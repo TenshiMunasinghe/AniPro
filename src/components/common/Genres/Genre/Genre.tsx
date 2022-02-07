e08166f70276c7cf36d54a54ba16e1e238843f96
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-02-07 19:55:28 +0530

    adjust padding

--- src/components/common/Genres/Genre/Genre.tsx
+++ src/components/common/Genres/Genre/Genre.tsx
@@ -6,16 +6,16 @@ interface Props {
   canInteract?: boolean
 }
 
-const style =
-  'text-[color:var(--color-adjusted)] flex items-center py-1 px-4 bg-zinc-200/50 dark:bg-zinc-700/50 rounded-lg border-2 border-[color:var(--color-adjusted)] whitespace-nowrap transition-all hocus:text-zinc-200 dark:hocus:text-zinc-700 hocus:!bg-[color:var(--color-adjusted)]'
+const className =
+  'text-[color:var(--color-adjusted)] flex items-center py-[0.25em] px-[1.25em] bg-zinc-200/50 dark:bg-zinc-700/50 rounded-lg border-2 border-[color:var(--color-adjusted)] whitespace-nowrap transition-all cursor-pointer hocus:text-zinc-200 dark:hocus:text-zinc-700 hocus:!bg-[color:var(--color-adjusted)]'
 
 const Genre = ({ genre, canInteract = true }: Props) => {
   return canInteract ? (
-    <Link className={style} to={linkToSearchPage({ genres: [genre] })}>
+    <Link className={className} to={linkToSearchPage({ genres: [genre] })}>
       {genre}
     </Link>
   ) : (
-    <div className={style}>{genre}</div>
+    <div className={className}>{genre}</div>
   )
 }
 
