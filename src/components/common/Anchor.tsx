b452d55896dc7d01291e000d7e961bdf69af3f84
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-02-07 17:52:18 +0530

    adjust colors

--- src/components/common/Anchor.tsx
+++ src/components/common/Anchor.tsx
@@ -7,7 +7,7 @@ const Anchor = ({ text, href }: Props) => {
   return (
     <a
       href={href}
-      className='text-zinc-900 underline underline-offset-1 hocus:text-teal-500 dark:text-zinc-400 dark:hocus:text-zinc-200'>
+      className='underline underline-offset-1 hocus:text-teal-500 dark:hocus:text-zinc-100'>
       {text}
     </a>
   )
