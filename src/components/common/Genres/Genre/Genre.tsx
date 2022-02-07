3cb8ea3e6998d1989aedbc435e644c0250394f5c
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-02-07 20:03:13 +0530

    increase border radius

--- src/components/common/Genres/Genre/Genre.tsx
+++ src/components/common/Genres/Genre/Genre.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const className =
-  'text-[color:var(--color-adjusted)] flex items-center py-[0.25em] px-[1.25em] bg-zinc-200/50 dark:bg-zinc-700/50 rounded-lg border-2 border-[color:var(--color-adjusted)] whitespace-nowrap transition-all cursor-pointer hocus:text-zinc-200 dark:hocus:text-zinc-700 hocus:!bg-[color:var(--color-adjusted)]'
+  'text-[color:var(--color-adjusted)] flex items-center py-[0.25em] px-[1.25em] bg-zinc-200/50 dark:bg-zinc-700/50 rounded-full border-2 border-[color:var(--color-adjusted)] whitespace-nowrap transition-all cursor-pointer hocus:text-zinc-200 dark:hocus:text-zinc-700 hocus:!bg-[color:var(--color-adjusted)]'
 
 const Genre = ({ genre, canInteract = true }: Props) => {
   return canInteract ? (
