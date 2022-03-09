70a6f1e0f7db1e73015a7ba6c36736399ae5c5c1
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-03-09 14:22:47 +0530

    center content

--- src/components/media/LoadMoreGrid.tsx
+++ src/components/media/LoadMoreGrid.tsx
@@ -3,7 +3,7 @@ import LoadMore, { LoadMoreProps } from '../common/LoadMore/LoadMore'
 
 const LoadMoreGrid: FC<LoadMoreProps> = ({ children, ...props }) => {
   return (
-    <div className='flex h-min flex-col justify-center space-y-5'>
+    <div className='flex h-min flex-col items-center space-y-5'>
       <div className='media-content--grid'>{children}</div>
       <LoadMore {...props} />
     </div>
