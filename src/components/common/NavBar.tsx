84d7ad18ab80354f8c9427670996edf9752cd3c3
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-03-19 12:11:13 +0530

    get rid of heading element

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -24,9 +24,11 @@ const NavBar = ({ position = 'static' }: Props) => {
   return (
     <nav
       className={`${position} bottom-0 z-50 flex w-full items-center space-x-4 bg-zinc-100 py-2 px-4 shadow shadow-zinc-200 dark:bg-zinc-700 dark:text-zinc-200 dark:shadow-zinc-900 sm:space-x-6 sm:px-6 lg:bottom-auto lg:top-0`}>
-      <h1 className='mr-auto hocus:text-teal-700 dark:hocus:text-teal-400 sm:text-xl'>
-        <Link to='/'>AniPro</Link>
-      </h1>
+      <Link
+        to='/'
+        className='mr-auto hocus:text-teal-700 dark:hocus:text-teal-400 sm:text-xl'>
+        AniPro
+      </Link>
       <BrowseOptions />
       <Switch
         onChange={onChange}
