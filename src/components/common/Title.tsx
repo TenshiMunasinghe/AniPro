7a4628cf7151104f982aff77db3849c751e05026
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-03-21 13:48:38 +0530

    add fallback to colors

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -14,7 +14,7 @@ const Title = ({ link, text, className }: Props) => {
     <Link
       to={link || '#'}
       className={classnames(
-        'transition-all line-clamp-2 hocus:text-[color:var(--color-adjusted)] dark:hocus:text-[color:var(--color-adjusted)]',
+        'transition-all line-clamp-2 hocus:text-[color:var(--color-adjusted),rgb(39,39,42)] dark:hocus:text-[color:var(--color-adjusted,rgb(244,244,245))]',
         className
       )}>
       {text}
